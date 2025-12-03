b2caa1979f4496bdc1d8ced9feaed4d82ec5f2a1
João Vitor Fonseca <joaovitorfonsouza@gmail.com>
2025-12-02 22:01:27 -0300

    Update 1148.c

--- 1148.c
+++ 1148.c
@@ -1,54 +1,65 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <string.h>
 
-#define MAXN 500
+#define MAXN 501
 #define INF 1000000000
 
-int dist[MAXN + 1][MAXN + 1];
+int mat[MAXN][MAXN];
 
-void floyd_warshall(int N) {
-    for (int k = 1; k <= N; k++)
-        for (int i = 1; i <= N; i++)
-            for (int j = 1; j <= N; j++)
-                if (dist[i][k] < INF && dist[k][j] < INF)
-                    if (dist[i][j] > dist[i][k] + dist[k][j])
-                        dist[i][j] = dist[i][k] + dist[k][j];
-    
-    // Conectar cidades no mesmo país (caminho bidirecional)
-    for (int i = 1; i <= N; i++)
-        for (int j = 1; j <= N; j++)
-            if (dist[i][j] != INF && dist[j][i] != INF)
-                dist[i][j] = dist[j][i] = (dist[i][j] < dist[j][i]) ? dist[i][j] : dist[j][i];
+int dijkstra(int n, int src, int dest) {
+    int dist[MAXN], vis[MAXN];
+    int i, j;
+    for (i = 0; i < n; i++) { dist[i] = INF; vis[i] = 0; }
+    dist[src] = 0;
+
+    for (i = 0; i < n; i++) {
+        int u = -1, best = INF;
+        for (j = 0; j < n; j++) {
+            if (!vis[j] && dist[j] < best) { best = dist[j]; u = j; }
+        }
+        if (u == -1) break;
+        if (u == dest) break;
+        vis[u] = 1;
+        for (j = 0; j < n; j++) {
+            if (!vis[j] && mat[u][j] != INF) {
+                if (dist[u] + mat[u][j] < dist[j]) dist[j] = dist[u] + mat[u][j];
+            }
+        }
+    }
+
+    if (dist[dest] >= INF) return -1;
+    return dist[dest];
 }
 
 int main() {
-    int N, E;
-    while (scanf("%d %d", &N, &E) && (N || E)) {
-        for (int i = 1; i <= N; i++)
-            for (int j = 1; j <= N; j++)
-                dist[i][j] = (i == j) ? 0 : INF;
-        
-        for (int i = 0; i < E; i++) {
-            int X, Y, H;
-            scanf("%d %d %d", &X, &Y, &H);
-            if (dist[Y][X] != INF) {
-                dist[X][Y] = dist[Y][X] = 0;
+    int n, e;
+    while (scanf("%d %d", &n, &e) == 2 && (n || e)) {
+        int i, j;
+        for (i = 0; i < n; i++)
+            for (j = 0; j < n; j++)
+                mat[i][j] = INF;
+
+        for (i = 0; i < e; i++) {
+            int u, v, w;
+            scanf("%d %d %d", &u, &v, &w);
+            u--; v--;
+            if (mat[v][u] != INF) {
+                
+                mat[u][v] = 0;
+                mat[v][u] = 0;
             } else {
-                dist[X][Y] = H;
+                if (w < mat[u][v]) mat[u][v] = w; 
             }
         }
-        
-        floyd_warshall(N);
-        
-        int K;
-        scanf("%d", &K);
-        while (K--) {
-            int O, D;
-            scanf("%d %d", &O, &D);
-            if (dist[O][D] == INF)
-                printf("Nao e possivel entregar a carta\n");
-            else
-                printf("%d\n", dist[O][D]);
+
+        int q;
+        scanf("%d", &q);
+        for (i = 0; i < q; i++) {
+            int a, b;
+            scanf("%d %d", &a, &b);
+            int ans = dijkstra(n, a - 1, b - 1);
+            if (ans == -1) printf("Nao e possivel entregar a carta\n");
+            else printf("%d\n", ans);
         }
         printf("\n");
     }
